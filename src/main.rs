3e03078becbf3c2a1f7c8df66907f7d958913f80
grizeldi <grizeldi@gmail.com>
2024-08-05 01:07:57 +0200

    Added ffmpeg band filtering

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 mod window;
 
 use std::env;
+use std::str;
+use std::process::Command;
 use claxon::FlacReader;
 
 const GAIN : i32 = 1;
 const WAVEFORM_SECONDS : usize = 60;
+const FILENAME_LOW_BAND : &str = "/tmp/waves_low.flac";
+const FILENAME_MID_BAND : &str = "/tmp/waves_mid.flac";
+const FILENAME_HIGH_BAND : &str = "/tmp/waves_high.flac";
 
 fn main() {
     // Find file
     let args : Vec<String> = env::args().collect();
     let path_to_open = &args[1];
+    separate_bands(path_to_open);
 
     // Open the file
     let mut flac_reader = FlacReader::open(path_to_open).expect("Failed to open FLAC stream.");
@@ -46,4 +52,36 @@ fn main() {
     while window.is_window_open() {
         window.update();
     }
+}
+
+fn separate_bands(filename : &String) {
+    // Generate the filtered versions
+    /*let output = */Command::new("ffmpeg")
+        .arg("-i")
+        .arg(filename)
+        .arg("-af")
+        .arg("lowpass=f=100")
+        .arg(FILENAME_LOW_BAND)
+        .output()
+        .expect("Failed to run ffmpeg");
+    // println!("{}", str::from_utf8(output.stdout.as_slice()).expect("Failed to format output"));
+    // println!("{}", str::from_utf8(output.stderr.as_slice()).expect("Failed to format stderr"));
+
+    Command::new("ffmpeg")
+        .arg("-i")
+        .arg(filename)
+        .arg("-af")
+        .arg("highpass=f=10000")
+        .arg(FILENAME_HIGH_BAND)
+        .output()
+        .expect("Failed to run ffmpeg");
+
+    Command::new("ffmpeg")
+        .arg("-i")
+        .arg(filename)
+        .arg("-af")
+        .arg("bandpass=f=1000")
+        .arg(FILENAME_MID_BAND)
+        .output()
+        .expect("Failed to run ffmpeg");
 }
