f5a5c3d0cddf5eeac2ebdf2242dcdec622068156
grizeldi <grizeldi@gmail.com>
2025-02-09 20:05:26 +0100

    Added proxy file opening code just so GTK stops erroring

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ mod waveformwidget;
 
 use epoxy::*;
 use gtk::glib;
-use gtk::prelude::{ApplicationExt, ApplicationExtManual, GtkWindowExt};
-use log::{debug, info};
+use gtk::prelude::{ApplicationExt, ApplicationExtManual, FileExt, GtkWindowExt};
+use log::{debug, error, info, warn};
 use std::ptr;
+use gtk::gio::ApplicationFlags;
 use crate::waveformwidget::WaveformWidget;
 
 fn main() -> glib::ExitCode {
@@ -32,19 +33,34 @@ fn main() -> glib::ExitCode {
     debug!("Creating application window.");
     let application = gtk::Application::builder()
         .application_id("com.grizeldi.Waves")
+        .flags(ApplicationFlags::HANDLES_OPEN)
         .build();
-    application.connect_startup(|app| {
-        let window = gtk::ApplicationWindow::builder()
-            .application(app)
-            .title("Waves")
-            .default_width(1280)
-            .default_height(720)
-            .build();
-        let waveform_display = WaveformWidget::new();
-        window.set_child(Some(&waveform_display));
-        window.present();
+    application.connect_open(|_, files, _| {
+        if files.len() == 0 {
+            error!("Opening zero files??");
+            return;
+        }
+        if files.len() > 1 {
+            warn!("Requested to open multiple files. This is not supported, only opening the first one.");
+        }
+        let file = &files[0];
+        info!("Opening file {:?}", file.path().unwrap().into_os_string());
+        //TODO open file properly
     });
+    application.connect_startup(build_ui);
     application.run();
 
     glib::ExitCode::SUCCESS
+}
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindow::builder()
+        .application(application)
+        .title("Waves")
+        .default_width(1280)
+        .default_height(720)
+        .build();
+    let waveform_display = WaveformWidget::new();
+    window.set_child(Some(&waveform_display));
+    window.present();
 }
