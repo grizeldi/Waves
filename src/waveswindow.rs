41ee4314b25e7732e624a281773583576fe0fbac
grizeldi <grizeldi@gmail.com>
2025-03-25 02:44:06 +0100

    Added song cover loading

--- src/waveswindow.rs
+++ src/waveswindow.rs
@@ -1,8 +1,10 @@
 use gtk::Application;
 use gtk::glib::{wrapper, Object};
+use gtk::prelude::WidgetExt;
 use gtk::subclass::prelude::ObjectSubclassIsExt;
-use lofty::prelude::*;
-use log::info;
+use lofty::picture::PictureType;
+use lofty::prelude::{Accessor, TaggedFileExt};
+use log::{debug, info, warn};
 
 mod imp {
     use gtk::{glib, CompositeTemplate, TemplateChild};
@@ -74,6 +76,41 @@ impl WavesWindow {
             }
             if let Some(author) = tag.artist() {
                 self.imp().author_label.set_label(&author.to_string());
+            }
+            if let Some(cover) = tag.get_picture_type(PictureType::CoverFront) {
+                let image_data = gtk::glib::Bytes::from(cover.data());
+                let stream = gtk::gio::MemoryInputStream::from_bytes(&image_data);
+
+                if let Ok(pixbuf) = gtk::gdk_pixbuf::Pixbuf::from_stream(&stream, gtk::gio::Cancellable::NONE) {
+                    let width = pixbuf.width();
+                    let height = pixbuf.height();
+                    let ratio = width as f32 / height as f32;
+
+                    let w: i32;
+                    let h: i32;
+                    let cover_size;
+                    if self.imp().album_cover_image.height() > 0 {
+                        cover_size = self.imp().album_cover_image.height();
+                    } else {
+                        cover_size = 70; //Hardcoded default size
+                    }
+
+                    if ratio > 1.0 {
+                        w = cover_size;
+                        h = (cover_size as f32 / ratio) as i32;
+                    } else {
+                        w = (cover_size as f32 * ratio) as i32;
+                        h = cover_size;
+                    }
+
+                    debug!("Cover size {width} x {height} (ratio: {ratio}), scaled down to: {w} x {h}.");
+
+                    let loaded_texture = pixbuf.scale_simple(w, h, gtk::gdk_pixbuf::InterpType::Nearest);
+                    let texture = loaded_texture.as_ref().map(gtk::gdk::Texture::for_pixbuf);
+                    self.imp().album_cover_image.set_paintable(texture.as_ref());
+                } else {
+                    warn!("Unable to load cover art");
+                }
             }
         }
 
